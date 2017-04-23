0e528afb2b575d5fbd02a368d221b74cf57b6350
Regivaldo Fraga do Nascimento <regifraga@gmail.com>
2017-04-23 17:15:24 -0300

    Updates about bug and some improvements
    Fixed bug in Receive method thah some time throws "WSACancelBlockingCall" exception.
    Updates about ISimpleUDPMessage.

--- SimpleUDPMessage/SimpleUDPMessageBase.cs
+++ SimpleUDPMessage/SimpleUDPMessageBase.cs
@@ -1,20 +1,22 @@
-﻿using System;
+using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RegiFraga.Comunication.UDP
+namespace IMS.UDP.Simple.Message
 {
     public abstract class SimpleUDPMessageBase : ISimpleUDPMessage
     {
         protected delegate void ReceiveHandle(IPEndPoint sender, string message);
         protected event ReceiveHandle OnReceive;
+        private Exception _lastError;
 
         private Task _task;
         private bool _cancel = false;
 
         public int Port { get; protected set; }
+        public bool HasError { get { return (_lastError != null); } }
         public UdpClient Udp { get; protected set; }
 
         protected abstract void InitializerUdp();
@@ -30,7 +32,7 @@ namespace RegiFraga.Comunication.UDP
                 }
             });
 
-            _task = new Task(readInput);
+            _task = new Task(readInput, new System.Threading.CancellationToken());
 
             _task.Start();
         }
@@ -38,7 +40,14 @@ namespace RegiFraga.Comunication.UDP
         public virtual void Stop()
         {
             _cancel = true;
+            Udp.Client.Close();
+
             _task.Wait();
+        }
+
+        public Exception GetLastError()
+        {
+            return _lastError;
         }
 
         public virtual void Dispose()
@@ -46,17 +55,32 @@ namespace RegiFraga.Comunication.UDP
             Stop();
 
             if (_task != null) _task.Dispose();
+
+            if (Udp.Client != null) Udp.Client.Dispose();
         }
 
         protected virtual void Receive()
         {
-            var sender = new IPEndPoint(IPAddress.Any, Port);
-            var data = new byte[1024];
+            try
+            {
+                var sender = new IPEndPoint(IPAddress.Any, Port);
+                var data = new byte[1024];
 
-            data = Udp.Receive(ref sender);
-            var message = Encoding.ASCII.GetString(data, 0, data.Length);
+                var receive = Udp.BeginReceive(null, null);
+                data = Udp.EndReceive(receive, ref sender);
 
-            OnReceive?.Invoke(sender, message);
+                var message = Encoding.ASCII.GetString(data, 0, data.Length);
+
+                OnReceive?.Invoke(sender, message);
+            }
+            catch (SocketException ex) //Timeout, WSACancelBlockingCall or some other error happened.
+            {
+                _lastError = ex;
+            }
+            catch (Exception ex)
+            {
+                _lastError = ex;
+            }
         }
     }
 }
