fb4e0f14b2937ce4febd8e35e3febe9847609ed1
Regivaldo Fraga do Nascimento <regifraga@gmail.com>
2017-04-23 17:08:24 -0300

    Included option to get last exception

--- SimpleUDPMessage/ISimpleUDPMessage.cs
+++ SimpleUDPMessage/ISimpleUDPMessage.cs
@@ -1,12 +1,14 @@
-﻿using System;
+using System;
 
 namespace RegiFraga.Comunication.UDP
 {
     public interface ISimpleUDPMessage : IDisposable
     {
         int Port { get; }
+        bool HasError { get; }
 
         void Start();
         void Stop();
+        Exception GetLastError();
     }
 }
